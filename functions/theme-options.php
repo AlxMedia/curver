7e3774b66305a8fbd3c3e7eff84f144e08d4cbfe
Alexander Agnarson <alexander.agnarson@gmail.com>
2019-02-17 14:28:35 +0100

    1.0.1

--- functions/theme-options.php
+++ functions/theme-options.php
@@ -10,6 +10,24 @@ Kirki::add_config( 'curver', array(
 	'option_type'   => 'theme_mod',
 ) );
 
+/*  Add Links
+/* ------------------------------------ */
+Kirki::add_section( 'morelink', array(
+	'title'       => esc_html__( 'AlxMedia', 'curver' ),
+	'type'        => 'link',
+	'button_text' => esc_html__( 'View More Themes', 'curver' ),
+	'button_url'  => 'http://alxmedia.se/themes/',
+	'priority'    => 13,
+) );
+Kirki::add_section( 'reviewlink', array(
+	'title'       => esc_html__( 'Like This Theme?', 'curver' ),
+	'panel'       => 'options',
+	'type'        => 'link',
+	'button_text' => esc_html__( 'Write a Review', 'curver' ),
+	'button_url'  => 'https://wordpress.org/support/theme/curver/reviews/?filter=5#new-post',
+	'priority'    => 1,
+) );
+
 /*  Add Panels
 /* ------------------------------------ */
 Kirki::add_panel( 'options', array(

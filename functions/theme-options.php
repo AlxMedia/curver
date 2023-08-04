cbfd4f27e4866108459a44dbaa87a4b549b58e39
AlxMedia <contact@alx.media>
2023-08-04 13:08:10 +0200

    Update to Font Awesome 6.4.2

--- functions/theme-options.php
+++ functions/theme-options.php
@@ -562,7 +562,7 @@ Kirki::add_field( 'curver_theme', array(
 	'label'			=> esc_html__( 'Create Social Links', 'curver' ),
 	'description'	=> esc_html__( 'Create and organize your social links', 'curver' ),
 	'section'		=> 'social',
-	'tooltip'		=> esc_html__( 'Font Awesome names:', 'curver' ) . ' <a href="https://fontawesome.com/v5/search?s=brands" target="_blank"><strong>' . esc_html__( 'View All', 'curver' ) . ' </strong></a>',
+	'tooltip'		=> esc_html__( 'Font Awesome names:', 'curver' ) . ' <a href="https://fontawesome.com/search?o=r&m=free&f=brands" target="_blank"><strong>' . esc_html__( 'View All', 'curver' ) . ' </strong></a>',
 	'row_label'		=> array(
 		'type'	=> 'text',
 		'value'	=> esc_html__('social link', 'curver' ),
@@ -579,7 +579,7 @@ Kirki::add_field( 'curver_theme', array(
 		'social-icon'	=> array(
 			'type'			=> 'text',
 			'label'			=> esc_html__( 'Icon Name', 'curver' ),
-			'description'	=> esc_html__( 'Font Awesome icons. Ex: fa-facebook ', 'curver' ) . ' <a href="https://fontawesome.com/v5/search?s=brands" target="_blank"><strong>' . esc_html__( 'View All', 'curver' ) . ' </strong></a>',
+			'description'	=> esc_html__( 'Font Awesome icons. Ex: fa-facebook ', 'curver' ) . ' <a href="https://fontawesome.com/search?o=r&m=free&f=brands" target="_blank"><strong>' . esc_html__( 'View All', 'curver' ) . ' </strong></a>',
 			'default'		=> 'fa-',
 		),
 		'social-link'	=> array(
